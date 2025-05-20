3eaff117d58dfca32fc63ea6d5a180e75965d007
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-05-20 13:59:43 -0300

    quase terminando o curso

--- first-ng-app/src/app/app.routes.ts
+++ first-ng-app/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import('./home/home.component').then(
+                m => m.HomeComponent
+            )
+        },
+    },
+    {
+        path: 'todos',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import('./components/todos/todos.component').then(
+                m => m.TodosComponent
+            )
+        },
+    }
+];
